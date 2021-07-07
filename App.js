e8ad921a13ae9b835f67dc36b0a43f09176ae9dc
Sylvia Antons <67543909+sylviaantons@users.noreply.github.com>
2021-07-07 09:31:50 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "App.css";
+
+function App() {
+    return (
+    <div className="App">
+        <header className="App-header">
+            <h1>Hello </h1>
+            <Weather />
+        </header>
+    </div>
+    );
+}
+
+export default App;
