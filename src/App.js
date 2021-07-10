3324fbcc1a6e9687b769da0a98c2b340b2f7d9c1
Sylvia Antons <67543909+sylviaantons@users.noreply.github.com>
2021-07-10 17:20:40 +0200

    link to github added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <h3>
+          <a href="https://github.com/sylviaantons/vanilla.js">Open-source</a>
+          coded with 💜 by Sylvia
+        </h3>
+        <h3>
+          Hosted on
+          <a href="https://www.netlify.com/">Netlify</a>
+        </h3>
     </div>
   );
 }
